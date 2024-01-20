acc77ca51ce99fef52939e449f8a78cc80cde5e2
FAKEvstheworld <agnese@yandex.ru>
2024-01-20 12:36:30 +0500

    KS-1-1 add

---
+++ src/main/kotlin/lesson_1/lesson1_task1.kt
@@ -0,0 +1,7 @@
+package lesson_1
+
+fun main() {
+    val thanksForOrder: String = "Благодарим за покупку!"
+    var numOrders: Int = 0
+    println("Количество заказов: $numOrders. $thanksForOrder")
+}
