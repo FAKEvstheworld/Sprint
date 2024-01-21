ce10a13cc342a8aa60e93557c8b0509079c693e3
FAKEvstheworld <agnese@yandex.ru>
2024-01-21 13:02:38 +0500

    KS-1-3-corrected

--- src/main/kotlin/lesson_1/lesson1_task3.kt
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -1,18 +1,17 @@
 package lesson_1
 
 fun main(){
-    val year: Int = 1961
-    var hour: Int = 9
-    var minute: Int = 7
-    // есть какой-то способ задавать перменным значения 09 и 07, без использования String?
+    val year = "1961"
+    var hour = "9"
+    var minute = "7"
 
     println("""$year
         |$hour
         |$minute
     """.trimMargin())
 
-    hour = 10
-    minute = 55
+    hour = "10"
+    minute = "55"
 
     println("Время посадки: $hour:$minute")
 }
