c602aa10d7e4e98d849d9631b07bc2bc428b843f
FAKEvstheworld <agnese@yandex.ru>
2024-01-24 11:42:15 +0500

    KS-3-3-MultiplicationTable

---
+++ src/main/kotlin/lesson_3/lesson3_task3.kt
@@ -0,0 +1,19 @@
+package lesson_3
+
+fun main() {
+
+    val enteredNumber = 3
+
+    println("""
+        $enteredNumber x 1 = ${enteredNumber * 1}
+        $enteredNumber x 2 = ${enteredNumber * 2}
+        $enteredNumber x 3 = ${enteredNumber * 3}
+        $enteredNumber x 4 = ${enteredNumber * 4}
+        $enteredNumber x 5 = ${enteredNumber * 5}
+        $enteredNumber x 6 = ${enteredNumber * 6}
+        $enteredNumber x 7 = ${enteredNumber * 7}
+        $enteredNumber x 8 = ${enteredNumber * 8}
+        $enteredNumber x 9 = ${enteredNumber * 9}
+    """.trimIndent())
+
+}
