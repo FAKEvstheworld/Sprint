8df952dc3bdcaf5b729b7a413ea008b6b55f0549
FAKEvstheworld <agnese@yandex.ru>
2024-01-24 10:23:01 +0500

    KS-2-3-FIXED

--- src/main/kotlin/lesson_2/lesson2_task3.kt
+++ src/main/kotlin/lesson_2/lesson2_task3.kt
@@ -8,11 +8,12 @@ fun main() {
     val minuteOfDeparture: Byte = 39
     val travelTime: Short = 457
 
-    val departureTime = hourOfDeparture* MINUTES_IN_A_HOUR+minuteOfDeparture // время отправления конвертированное в минуты (579)
-    val arrivalTime = departureTime+travelTime // время прибытия в минутах (1036)
+    val departureTime =
+        hourOfDeparture * MINUTES_IN_A_HOUR + minuteOfDeparture // время отправления конвертированное в минуты (579)
+    val arrivalTime = departureTime + travelTime // время прибытия в минутах (1036)
 
-    val arrivalHour = arrivalTime/ MINUTES_IN_A_HOUR // час прибытия (17)
-    val arrivalMinute = arrivalTime/ MINUTES_IN_A_HOUR% MINUTES_IN_A_HOUR // минута прибытия (17)
+    val arrivalHour = arrivalTime / MINUTES_IN_A_HOUR // час прибытия (17)
+    val arrivalMinute = arrivalTime % MINUTES_IN_A_HOUR // минута прибытия (16)
 
     println("Время прибытия: $arrivalHour:$arrivalMinute")
 
