da12048683d1dd5463e70d3e15235a3f87360b06
FAKEvstheworld <agnese@yandex.ru>
2024-01-24 10:27:25 +0500

    KS-2-4-FIXED

--- src/main/kotlin/lesson_2/lesson2_task4.kt
+++ src/main/kotlin/lesson_2/lesson2_task4.kt
@@ -4,19 +4,14 @@ fun main() {
 
     val crystalOre = 7
     val ironOre = 11
-//    println("Без баффа добыто $crystalOre кристаллической и $ironOre железной руды")
 
-    val buffSize = 1.2
-    val crystalOreByBuff = crystalOre*buffSize-crystalOre
-    val ironOreByBuff = ironOre*buffSize-ironOre
+    val buffSize = 20
+    val percent = 100
+
+    val crystalOreByBuff = crystalOre * (buffSize.toFloat() / percent)
+    val ironOreByBuff = ironOre * (buffSize.toFloat() / percent)
     println("""Добыто бонусной руды с баффом: 
         |${crystalOreByBuff.toInt()} кристаллической
         |${ironOreByBuff.toInt()} железной""".trimMargin())
-
-// Объявил 20% как 1.2, так можно или нужно объявлять как 20:
-//    val crystalOre = 7
-//    val buffSize = 20
-//    val crystalOreByBuff = crystalOre1 * (buffSize1.toFloat() / 100)
-//    println(crystalOreByBuff.toInt()) // 1
 
 }
