e9865a5ae852a23bc444b9bd115c6f57371f755c
FAKEvstheworld <agnese@yandex.ru>
2024-01-25 19:44:51 +0500

    KS-3-1-Fixed

--- src/main/kotlin/lesson_3/lesson3_task1.kt
+++ src/main/kotlin/lesson_3/lesson3_task1.kt
@@ -2,13 +2,13 @@ package lesson_3
 
 fun main() {
 
-    val eveningGreeting = "Добрый вечер, "
-    val dayGreeting = "Добрый день, "
+    val eveningGreeting = "Добрый вечер"
+    val dayGreeting = "Добрый день"
     val username = "Игорь"
 
     val time = 19
 
-    val greeting = if (time > 18) eveningGreeting + username else dayGreeting + username
+    val greeting = if (time > 18) "$eveningGreeting, $username" else "$dayGreeting, $username"
 
     println(greeting)
 
