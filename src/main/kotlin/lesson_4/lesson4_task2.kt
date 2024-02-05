efc0448bce5bb5398764dd3f6dbe69d7b07a64b2
FAKEvstheworld <agnese@yandex.ru>
2024-02-05 12:02:43 +0500

    KS-4-2-fixed

--- src/main/kotlin/lesson_4/lesson4_task2.kt
+++ src/main/kotlin/lesson_4/lesson4_task2.kt
@@ -1,19 +1,13 @@
 package lesson_4
 
+    const val minWeight = 35
+    const val maxWeight = 100
+    const val maxVolume = 100
+
 fun main() {
 
-    val minWeight = 35
-    val maxWeight = 100
-    val maxVolume = 100
-
-    var cargoWeight = 42
-    var cargoVolume = 120
-
-    println("Груз с весом $cargoWeight кг и объемом $cargoVolume л соответствует категории 'Average': " +
-            "${((minWeight < cargoWeight) and (cargoWeight <= maxWeight)) and (cargoVolume < maxVolume)}")
-
-    cargoWeight = 20
-    cargoVolume = 80
+    var cargoWeight = 20
+    var cargoVolume = 80
 
     println("Груз с весом $cargoWeight кг и объемом $cargoVolume л соответствует категории 'Average': " +
             "${((minWeight < cargoWeight) and (cargoWeight <= maxWeight)) and (cargoVolume < maxVolume)}")
