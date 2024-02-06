32712bfb4fc060bd19799541dae3c45b1dccd94e
FAKEvstheworld <agnese@yandex.ru>
2024-02-06 14:14:54 +0500

    KS-4-1-fixed

--- src/main/kotlin/lesson_4/lesson4_task1.kt
+++ src/main/kotlin/lesson_4/lesson4_task1.kt
@@ -1,14 +1,14 @@
 package lesson_4
 
-const val tableCount = 13
+const val TABLE_COUNT = 13
 
 fun main() {
 
-    var reservedTableToday = 13
-    var reservedTableTom = 9
+    val reservedTableToday = 13
+    val reservedTableTom = 9
 
-    val availableToday = tableCount != reservedTableToday
-    val availableTom = tableCount != reservedTableTom
+    val availableToday = TABLE_COUNT != reservedTableToday
+    val availableTom = TABLE_COUNT != reservedTableTom
 
     println("Доступность столиков на сегодня: $availableToday\nДоступность столиков на завтра: $availableTom")
 //
