00a1462fbb8b4ad8dcc48fdae28e00e48ffe2f7e
FAKEvstheworld <agnese@yandex.ru>
2024-02-08 23:41:33 +0500

    KS-5-5-fixed

--- src/main/kotlin/lesson_5/lesson5_task5.kt
+++ src/main/kotlin/lesson_5/lesson5_task5.kt
@@ -1,7 +1,5 @@
 package lesson_5
 
-import kotlin.random.Random
-
 fun main() {
 
     val min = 0
@@ -22,14 +20,17 @@ fun main() {
 
     var inputList = listOf(inputNum1, inputNum2, inputNum3)
 
-    if ((inputNum1 >= min) and (inputNum1 <= max) and (inputNum2 >= min) and (inputNum2 <= max)
-        and (inputNum3 >= min) and (inputNum3 <= max)) {
+    if (inputNum1 >= min && inputNum1 <= max && inputNum2 >= min && inputNum2 <= max &&
+        inputNum3 >= min && inputNum3 <= max
+    ) {
         val commonNums = list.intersect(inputList)
-        if (commonNums.size == 3) println("Вы угадали все числа и выиграли джекпот!")
-        else if (commonNums.size == 2) println("Вы угадали два числа и получаете крупный приз!")
-        else if (commonNums.size == 1) println("Вы угадали одно число, вам выплачивается утешительный приз.")
-        else if (commonNums.size == 0) println("Вы не угадали ни одного числа")
+        when (commonNums.size) {
+            3 -> println("Вы угадали все числа и выиграли джекпот!")
+            2 -> println("Вы угадали два числа и получаете крупный приз!")
+            1 -> println("Вы угадали одно число, вам выплачивается утешительный приз.")
+            else -> println("Вы не угадали ни одного числа")
+        }
+
         println("Выигрышные числа были: $list")
-    }
-    else println("Числа нет подходят")
+    } else println("Числа нет подходят")
 }
