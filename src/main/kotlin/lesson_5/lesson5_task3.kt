ebd033084f650f1ecfce2306f5bc1d821b489d52
FAKEvstheworld <agnese@yandex.ru>
2024-02-08 23:31:28 +0500

    KS-5-3-fixed-2

--- src/main/kotlin/lesson_5/lesson5_task3.kt
+++ src/main/kotlin/lesson_5/lesson5_task3.kt
@@ -13,11 +13,11 @@ fun main() {
     println("Введите второе число: ")
     val inputNum2 = readln().toInt()
 
-    if ((inputNum1 >= 0) and (inputNum1 <= 42) and (inputNum2 >= 0) and (inputNum2 <= 42)) {
-        if (((inputNum1 == winNum1) or (inputNum1 == winNum2)) and ((inputNum2 == winNum1) or (inputNum2 == winNum2))
-            and (inputNum1 != inputNum2)
+    if (inputNum1 >= 0 && inputNum1 <= 42 && inputNum2 >= 0 && inputNum2 <= 42) {
+        if ((inputNum1 == winNum1 || inputNum1 == winNum2) && (inputNum2 == winNum1 || inputNum2 == winNum2) &&
+            inputNum1 != inputNum2
         ) println("Поздравляем! Вы выиграли главный приз!")
-        else if (((inputNum1 == winNum1) or (inputNum1 == winNum2)) or ((inputNum2 == winNum1) or (inputNum2 == winNum2)))
+        else if ((inputNum1 == winNum1 || inputNum1 == winNum2) || (inputNum2 == winNum1 || inputNum2 == winNum2))
             println("Вы выиграли утешительный приз! Выигрышные числа были $winNum1 и $winNum2")
         else println("Неудача! Выигрышные числа были $winNum1 и $winNum2")
     }
