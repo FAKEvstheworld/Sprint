54eb7ceb47953684d3607f4be82dcf85a7d02014
FAKEvstheworld <agnese@yandex.ru>
2024-02-14 11:49:24 +0500

    KS-8-3-

--- src/main/kotlin/lesson_8/lesson8_task3c.kt
+++ src/main/kotlin/lesson_8/lesson8_task3c.kt
@@ -1,2 +1,16 @@
 package lesson_8
 
+fun main() {
+
+    val arrayOfIngredients = arrayOf("хлеб", "колбаса", "майонез")
+
+    println("Введите название ингредиента: ")
+
+    val input = readln()
+
+    if (arrayOfIngredients.contains(input)) {
+        println("Ингредиент $input в рецепте есть")
+        return
+    } else println("Такого ингредиента в рецепте нет")
+
+}
