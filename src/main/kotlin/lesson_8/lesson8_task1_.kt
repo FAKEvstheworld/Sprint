4c779809803b7b99fe9385d87a66c0d87ca517bc
FAKEvstheworld <agnese@yandex.ru>
2024-02-15 12:40:53 +0500

    KS-8-1

---
+++ src/main/kotlin/lesson_8/lesson8_task1_.kt
@@ -0,0 +1,18 @@
+package lesson_8
+
+fun main() {
+
+    var advertisementsViewed = intArrayOf(0, 0, 0, 0, 0, 0, 0)
+    var weekViews = 0
+
+    for (i in 0 until advertisementsViewed.size) {
+        advertisementsViewed[i] = (0..10).random() // симуляция записи данных о просмотре рекламы
+    }
+
+    for (i in advertisementsViewed) {
+        println(i) // смотрим сколько просмотров стало после записи, по дням
+    }
+
+    println(advertisementsViewed.sum())
+
+}
