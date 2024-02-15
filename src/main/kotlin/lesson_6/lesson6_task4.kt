3d7578bd685556ed5e48efdb84ad25fada40cfc2
FAKEvstheworld <agnese@yandex.ru>
2024-02-15 13:12:47 +0500

    KS-6-4-fixed

--- src/main/kotlin/lesson_6/lesson6_task4.kt
+++ src/main/kotlin/lesson_6/lesson6_task4.kt
@@ -9,19 +9,15 @@ fun main() {
 
     while (attemptsCount > 0){
         println("Введите число: ")
+
         val inputNumber = readln().toInt()
-        if (inputNumber in 1..9){
-            if (inputNumber == secretNumber){
-                println("Вы угадали число!")
-                break
-            }
-            else {
-                attemptsCount--
-                println("Неверное число. Попыток осталось: $attemptsCount")
-            }
+
+        attemptsCount--
+
+        if (inputNumber == secretNumber){
+            println("Вы угадали число!")
+            break
         }
-        else println("неверное число")
-
+        else println("Неверное число. Попыток осталось: $attemptsCount")
     }
-
 }
