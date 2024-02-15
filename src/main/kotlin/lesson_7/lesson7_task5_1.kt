19335b97562dcf0321fb3bfa40f877f99cb87174
FAKEvstheworld <agnese@yandex.ru>
2024-02-15 13:53:27 +0500

    KS-7-5-1-fixed

--- src/main/kotlin/lesson_7/lesson7_task5_1.kt
+++ src/main/kotlin/lesson_7/lesson7_task5_1.kt
@@ -1,45 +1,36 @@
 package lesson_7
 
-const val MIN_PASSWORD_LENGTH = 6
+const val MIN_PASSWORD_LEN = 6
 fun main() {
 
     val numRange = '0'..'9'
     val letterRange = ('a'..'z')
     val capitalLetterRange = ('A'..'Z')
-    val range = numRange + numRange + numRange + letterRange + capitalLetterRange
     var password = ""
 
     println("Введите желаемое количество символов пароле: ")
 
     var passwordLength = readln().toInt()
 
-    while (passwordLength < MIN_PASSWORD_LENGTH) {
+    while (passwordLength < MIN_PASSWORD_LEN) {
         println("Пароль слишком короткий")
         passwordLength = readln().toInt()
     }
 
-    for (i in 1..passwordLength) {
-        if (password.length < passwordLength - 2) {
-            password = password + range.random()
-        }
-        else if (password.length >= passwordLength - 2) { // в пароль осталось добавить 2 символа
-            if (!password.any(numRange::contains)) {
-                password = password + numRange.random() // Если в пароле еще нет цифры, добавляем её
-            }
+    password = password + numRange.random() + letterRange.random() + capitalLetterRange.random() // сразу добавляем 3 нужных символа
 
-            else if (!password.any(letterRange::contains)) {
-                password = password + letterRange.random() // Если в пароле еще нет буквы, добавляем её
-            }
-
-            else if (!password.any(capitalLetterRange::contains)) {
-                password = password + capitalLetterRange.random() // Если в пароле еще нет заглавной буквы, добавляем её
-            }
-
-            else {
-                password = password + range.random() // если в пароле уже есть и цифра, и буква, и заглавная буква, то добавляем рандомный символ
-            }
+    for (i in 1..passwordLength-3) {
+        val rangeChoice= (0..2).random()
+        when (rangeChoice) { // позволяет добавлять символ каждого типа с одинаковым шансом
+            0 -> password += numRange.random()
+            1 -> password += letterRange.random()
+            2 -> password += capitalLetterRange.random()
         }
     }
+
+    val shufflePassword = password.toMutableList()
+    shufflePassword.shuffle()
+    password = shufflePassword.joinToString("") // другой простой путь для перемешивания символов в строке я не нашёл
 
     println(password)
 }
